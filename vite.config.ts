3e776450f035d26ea10780eba23e2633e99d82de
Dinesh Sandaruwan <imdineshsandaru@gmail.com>
2025-06-16 16:53:19 +0530

    Added custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/teacher-transfer-webapp/', // 👈 match your repo name
+  base: '', // 👈 match your repo name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
